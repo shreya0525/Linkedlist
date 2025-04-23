f3b23eb6397402a457e531ee439808d05b0a7eb1
shreya0525 <shreya.scs24@bmsce.ac.in>
2025-04-23 19:49:51 +0530

    Create link.java

---
+++ link.java
@@ -0,0 +1,49 @@
+public class LinkedListDemo {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkedList {
+        Node head;
+
+        // Add a node at the end
+        public void add(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                return;
+            }
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        // Print the list
+        public void printList() {
+            Node current = head;
+            while (current != null) {
+                System.out.print(current.data + " -> ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.print("Current list: ");
+        list.printList();
+    }
+}
